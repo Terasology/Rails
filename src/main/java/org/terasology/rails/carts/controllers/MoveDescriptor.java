e1021acd0dbfc748d0b1417a3713440e6a3a5d5f
Adeon <adeon@drom.ru>
2014-04-25 13:07:15 +1100

    Fixed pitch rotation for the -x and +z directions

--- src/main/java/org/terasology/rails/carts/controllers/MoveDescriptor.java
+++ src/main/java/org/terasology/rails/carts/controllers/MoveDescriptor.java
@@ -241,43 +241,49 @@ public class MoveDescriptor {
 
         minecart.pitch = 0;
 
+        Vector3f dir = new Vector3f(position);
+        dir.sub(motionState.prevPosition);
+
+        if (motionState.nextBlockIsSlope && (dir.x < 0 || dir.z > 0)) {
+            side = side.reverse();
+        }
+
         if (blockType.equals(ConnectsToRailsComponent.RAILS.SLOPE) || motionState.nextBlockIsSlope) {
             switch (side) {
                 case LEFT:
                 case BACK:
                     minecart.pitch = 45 * motionState.pitchSign;
-
-                    if (motionState.nextBlockIsSlope) {
-                        float targetY = motionState.currentBlockPosition.y + 0.5f;
-                        float sourceY = position.y - 0.5f;
-
-                        logger.info("targetY origin " + targetY);
-                        logger.info("sourceY " + sourceY);
-
-                        targetY = targetY - sourceY;
-                        logger.info("targetY before " + targetY);
-                        if ( targetY > 1 ) {
-                            targetY = 1;
-                        }
-                        float percent = 3.5f*(1 - targetY);
-
-                        if (percent > 1) {
-                            percent = 1;
-                        }
-                        logger.info("percent " + percent);
-                        minecart.pitch = minecart.pitch*percent;
-                        if (minecart.pitch > 45 || minecart.pitch < -45) {
-                            minecart.pitch = 45*Math.signum(minecart.pitch);
-                        }
-                    }
-                  // logger.info("Yaw is " + minecart.yaw + "Side is " + side);
                     break;
                 case RIGHT:
                 case FRONT:
-                    minecart.pitch = -48 * motionState.pitchSign;
-                    //logger.info("Yaw is " + minecart.yaw + "Side is " + side);
+                    minecart.pitch = -45 * motionState.pitchSign;
                     break;
             }
+
+            if (motionState.nextBlockIsSlope) {
+                float targetY = motionState.currentBlockPosition.y + 0.5f;
+                float sourceY = position.y - 0.5f;
+
+                //   logger.info("targetY origin " + targetY);
+                //   logger.info("sourceY " + sourceY);
+
+                targetY = targetY - sourceY;
+                //  logger.info("targetY before " + targetY);
+                if ( targetY > 1 ) {
+                    targetY = 1;
+                }
+                float percent = 3.5f*(1 - targetY);
+
+                if (percent > 1) {
+                    percent = 1;
+                }
+                //    logger.info("percent " + percent);
+                minecart.pitch = minecart.pitch*percent;
+                if (minecart.pitch > 45 || minecart.pitch < -45) {
+                    minecart.pitch = 45*Math.signum(minecart.pitch);
+                }
+            }
+            logger.info("Side is " + side);
         }
     }
 
