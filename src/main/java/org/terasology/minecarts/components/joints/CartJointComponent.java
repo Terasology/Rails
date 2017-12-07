7d0d44082429152fd91daf38b9681df0e8a7b1d4
Arav Singhal <singhal.arav@gmail.com>
2017-12-07 16:13:11 +0530

    Add impulse application

--- src/main/java/org/terasology/minecarts/components/joints/CartJointComponent.java
+++ src/main/java/org/terasology/minecarts/components/joints/CartJointComponent.java
@@ -16,15 +16,18 @@
 package org.terasology.minecarts.components.joints;
 
 import org.terasology.entitySystem.Component;
+import org.terasology.entitySystem.entity.EntityRef;
 import org.terasology.logic.location.LocationComponent;
 import org.terasology.math.geom.Vector3f;
+import org.terasology.minecarts.Constants;
 import org.terasology.minecarts.Util;
 import org.terasology.minecarts.components.RailVehicleComponent;
 import org.terasology.physics.components.RigidBodyComponent;
+import org.terasology.segmentedpaths.components.SegmentEntityComponent;
 
 public class CartJointComponent implements Component {
     // TODO: Make configurable?
-    public static final float JOINT_DISTANCE = 0.5f;
+    public static final float JOINT_DISTANCE = 1.0f;
 
     public CartJointSocket frontJointSocket;
     public CartJointSocket rearJointSocket;
@@ -51,6 +54,55 @@ public class CartJointComponent implements Component {
         }
     }
 
-    public void applyImpulse() {
+    private static void applySocketImpulse(CartJointSocket socket, float delta) {
+        CartJointSocket otherSocket = socket.getConnectingSocket();
+
+        if (socket.hasImpulseBeenApplied || otherSocket.hasImpulseBeenApplied) {
+            return;
+        }
+
+        EntityRef vehicle = otherSocket.connectingVehicle;
+        EntityRef otherVehicle = socket.connectingVehicle;
+
+        LocationComponent location = vehicle.getComponent(LocationComponent.class);
+        LocationComponent otherLocation = otherVehicle.getComponent(LocationComponent.class);
+
+        RailVehicleComponent railVehicle = vehicle.getComponent(RailVehicleComponent.class);
+        RailVehicleComponent otherRailVehicle = otherVehicle.getComponent(RailVehicleComponent.class);
+
+        SegmentEntityComponent segmentVehicle = vehicle.getComponent(SegmentEntityComponent.class);
+        SegmentEntityComponent otherSegmentVehicle = otherVehicle.getComponent(SegmentEntityComponent.class);
+
+        RigidBodyComponent rigidBody = vehicle.getComponent(RigidBodyComponent.class);
+        RigidBodyComponent otherRigidBody = otherVehicle.getComponent(RigidBodyComponent.class);
+
+        Vector3f worldSocketPoint = Util.localToWorldPosition(socket.localSocketPoint, location);
+        Vector3f worldOtherSocketPoint = Util.localToWorldPosition(otherSocket.localSocketPoint, otherLocation);
+
+        Vector3f normal = worldOtherSocketPoint.sub(worldSocketPoint);
+        float distance = normal.length();
+
+        float relVelAlongNormal = otherRailVehicle.velocity.dot(otherSegmentVehicle.heading) - railVehicle.velocity.dot(segmentVehicle.heading);
+        float inverseMassSum = 1 / rigidBody.mass + 1 / otherRigidBody.mass;
+        float bias = (Constants.BAUMGARTE_COFF / delta) * (distance - JOINT_DISTANCE);
+        float j = -(relVelAlongNormal + bias) / inverseMassSum;
+
+        railVehicle.velocity.sub(new Vector3f(segmentVehicle.heading).mul(j / rigidBody.mass));
+        otherRailVehicle.velocity.add(new Vector3f(otherSegmentVehicle.heading).mul(j / otherRigidBody.mass));
+
+        vehicle.saveComponent(railVehicle);
+        otherVehicle.saveComponent(otherRailVehicle);
+
+        socket.hasImpulseBeenApplied = true;
+        otherSocket.hasImpulseBeenApplied = true;
+    }
+
+    public void applyImpulse(float delta) {
+        if (frontJointSocket != null) {
+            applySocketImpulse(frontJointSocket, delta);
+        }
+        if (rearJointSocket != null) {
+            applySocketImpulse(rearJointSocket, delta);
+        }
     }
 }
