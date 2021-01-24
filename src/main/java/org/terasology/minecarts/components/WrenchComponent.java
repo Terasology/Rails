87a4176827e16b8bf208debdf0d822838185b66f
Michael Pollind <mpollind@gmail.com>
2021-01-23 16:56:53 -0800

    cleanup: remove unused imports methods and update copyright

--- src/main/java/org/terasology/minecarts/components/WrenchComponent.java
+++ src/main/java/org/terasology/minecarts/components/WrenchComponent.java
@@ -1,8 +1,7 @@
-// Copyright 2020 The Terasology Foundation
+// Copyright 2021 The Terasology Foundation
 // SPDX-License-Identifier: Apache-2.0
 
 package org.terasology.minecarts.components;
-
 
 import org.terasology.entitySystem.Component;
 import org.terasology.entitySystem.entity.EntityRef;
