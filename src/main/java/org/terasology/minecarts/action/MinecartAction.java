01a752b0e0959b3daca9fc4cad5129f93ef6f2ec
Michael Pollind <mpollind@gmail.com>
2021-03-14 17:51:38 -0700

    chore: migrate off of engine package
    pr: https://github.com/MovingBlocks/Terasology/issues/4580

--- src/main/java/org/terasology/minecarts/action/MinecartAction.java
+++ src/main/java/org/terasology/minecarts/action/MinecartAction.java
@@ -13,11 +13,11 @@ import org.terasology.engine.entitySystem.systems.BaseComponentSystem;
 import org.terasology.engine.entitySystem.systems.RegisterMode;
 import org.terasology.engine.entitySystem.systems.RegisterSystem;
 import org.terasology.engine.logic.common.ActivateEvent;
-import org.terasology.engine.logic.inventory.InventoryManager;
 import org.terasology.engine.logic.inventory.ItemComponent;
 import org.terasology.engine.registry.In;
 import org.terasology.engine.world.block.BlockComponent;
 import org.terasology.engine.world.block.BlockManager;
+import org.terasology.logic.inventory.InventoryManager;
 import org.terasology.minecarts.blocks.RailComponent;
 import org.terasology.minecarts.components.CartDefinitionComponent;
 
